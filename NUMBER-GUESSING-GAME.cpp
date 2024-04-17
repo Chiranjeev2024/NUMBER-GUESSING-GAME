0af246ca33406545666953ec448ffc79aaef40e2
Chiranjeev2024 <sks7369064258@gmail.com>
2024-04-17 16:21:10 +0530

    Rule book added

--- NUMBER-GUESSING-GAME.cpp
+++ NUMBER-GUESSING-GAME.cpp
@@ -4,5 +4,23 @@ using namespace std;
 
 int main(){
 
-    cout<<"\t\t\tWELCOME TO THE NUMBER GUESSING GAME"<<endl;
+    cout << "\nEnter the difficulty level: \n";
+        cout << "1 for easy!\t";
+        cout << "2 for medium!\t";
+        cout << "3 for difficult!\t";
+        cout << "0 for ending the game!\n" << endl;
+
+    cout<<"\t\t\t* WELCOME TO THE NUMBER GUESSING GAME *"<<endl<<endl;
+
+    cout<<"  # RULE BOOK"<<endl;
+    cout<<"\t* You have to guess a number between 1 and 100."<<endl;
+    cout<<"\t* You'll have limited choices based on the level you choose."<<endl;
+    cout << "\t - 1 for easy!\t"<<endl;
+    cout << "\t - 2 for medium!\t"<<endl;
+    cout << "\t - 3 for difficult!\t"<< endl<<endl;
+
+    cout<<"\t\t\t<------Good Luck!------>"<<endl;
+
+
+
 }
